c2815b0d52d537601cecfccfb910bb3a10cc89fb
srinivas-byte <55057599+srinivas-byte@users.noreply.github.com>
2021-03-24 18:05:29 +0530

    Add files via upload

---
+++ yuniql_test/v0.03/02-setup-data.sql
@@ -0,0 +1,7 @@
+insert into owners values(101);
+insert into owners values(102);
+insert into owners values(103);
+insert into owners values(104);
+insert into owners values(105);
+insert into owners values(106);
+
