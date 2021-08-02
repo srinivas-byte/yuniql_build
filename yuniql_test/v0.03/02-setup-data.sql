49af51a9399b07134666aad205cdd576882c2ae6
srinivas-byte <55057599+srinivas-byte@users.noreply.github.com>
2021-08-02 19:25:26 +0530

    Update 02-setup-data.sql

--- yuniql_test/v0.03/02-setup-data.sql
+++ yuniql_test/v0.03/02-setup-data.sql
@@ -4,4 +4,9 @@ insert into owners values(103);
 insert into owners values(104);
 insert into owners values(105);
 insert into owners values(106);
+insert into owners values(107);
+insert into owners values(108);
+insert into owners values(109);
+insert into owners values(110);
+insert into owners values(111);
 
