58d08e6d5a02c173cd71a25d54afeac664c434e2
srinivas-byte <55057599+srinivas-byte@users.noreply.github.com>
2021-08-02 19:05:35 +0530

    Update updatr.sql

--- yuniql_test/_post/updatr.sql
+++ yuniql_test/_post/updatr.sql
@@ -1,3 +1,3 @@
 update jobs set job_title='KILLER' where job_id=10;
-delete from jobs where job_title= 'KILLER'
+--delete from jobs where job_title= 'KILLER'
 ALTER TABLE jobs ADD Mobile_no VARCHAR(20) NOT NULL;  
