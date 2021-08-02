cd7d6b8e72afe1abd499d57bae65635e3a75e6ef
srinivas-byte <55057599+srinivas-byte@users.noreply.github.com>
2021-08-02 19:54:59 +0530

    Update updatr.sql

--- yuniql_test/_post/updatr.sql
+++ yuniql_test/_post/updatr.sql
@@ -1,4 +1,4 @@
 update jobs set job_title='KILLER' where job_id=10;
 delete from owners where location_id=101
-
+update  departments set random ='Killer is here' where department_id=102;
 --ALTER TABLE jobs ADD Mobile_no VARCHAR(20) ;  
