3e7e08eeffac8046c7ddbe81d00c0636136d33f6
srinivas-byte <55057599+srinivas-byte@users.noreply.github.com>
2021-08-02 12:49:49 +0530

    Update updatr.sql

--- yuniql_test/_post/updatr.sql
+++ yuniql_test/_post/updatr.sql
@@ -1,3 +1,4 @@
 update jobs set job_title='KILLER' where job_id=10;
 delete from jobs where job_title= 'KILLER'
-Alter table jobs add coloumn [DF_ProductModel_rowguid]  DEFAULT(getdate()) as DATE;
+ALTER TABLE jobs   
+ADD  date not null;   
