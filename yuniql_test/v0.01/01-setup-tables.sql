da84ba49ce35710f80742046e925cd70fa58c332
srinivas-byte <55057599+srinivas-byte@users.noreply.github.com>
2021-08-02 11:40:29 +0530

    Update 01-setup-tables.sql

--- yuniql_test/v0.01/01-setup-tables.sql
+++ yuniql_test/v0.01/01-setup-tables.sql
@@ -1,13 +1,13 @@
 CREATE TABLE departments (
-    department_id SERIAL PRIMARY KEY,
-    department_name CHARACTER VARYING (30) NOT NULL,
+    department_id Integer PRIMARY KEY,
+    department_name Varchar (30) NOT NULL,
     location_id INTEGER,
     FOREIGN KEY (location_id) REFERENCES locations (location_id)
 );
  
 CREATE TABLE jobs (
-    job_id SERIAL PRIMARY KEY,
+    job_id Integer PRIMARY KEY,
     job_title CHARACTER VARYING (35) NOT NULL,
-    min_salary NUMERIC (8, 2),
-    max_salary NUMERIC (8, 2)
+    min_salary Decimal (8, 2),
+    max_salary Decimal (8, 2)
 );
